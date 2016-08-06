74cf680e780ccccbe5e62c6611f4be40153aef88
Philon <therealphilon@gmail.com>
2016-08-07 00:04:38 +0200

    lewy psycho korytarz

---
+++ Assets/Scripts/Changer.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public delegate void ChangeMethod(float a);
+
+public class Changer : MonoBehaviour {
+
+	private float StartTime, DeltaTime, StartV, EndV;
+	private ChangeMethod ChangeMethod;
+	
+	public void Change(float startV, float endV, float time, ChangeMethod changeMethod) {
+		EndV = endV;
+		StartV = startV;
+		DeltaTime = time;
+		StartTime = Time.time;
+		ChangeMethod = changeMethod;
+	}
+	
+
+	void Update() {
+		if (StartTime != 0) {
+
+			if (Time.time > DeltaTime + StartTime) {
+				Destroy(this);
+			}
+			float percent = (Time.time - StartTime) / DeltaTime;
+			float deltaA = (EndV * percent);
+			float step = Mathf.SmoothStep(0.0f, 1.0f, percent);
+			float finalA = Mathf.Lerp(StartV, deltaA, step);
+
+			ChangeMethod(finalA);
+		}
+	}
+
+}
---
+++ Assets/Scripts/RotateAround.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateAround : MonoBehaviour {
+
+	public float speed = 30.0f;
+	public Transform pivot;
+	public Vector3 Side;
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log("vector up, " + Vector3.up + ", left: " + Vector3.left);
+	}
+	
+	void Update () {
+		float tmpSpeed = speed;
+		if (transform.position.y < -211 ) {
+			tmpSpeed *= 5;
+		}
+		
+		transform.RotateAround(pivot.position, Side, tmpSpeed * Time.deltaTime);
+ }
+}
