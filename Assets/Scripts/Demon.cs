784f2cdc1d2c73702a86e65f58c3d17ecd57e100
SteamFox <dot@steamfox.net>
2016-08-06 23:33:29 +0200

    Particles

--- Assets/Scripts/Demon.cs
+++ Assets/Scripts/Demon.cs
@@ -15,6 +15,10 @@ public class Demon : MonoBehaviour {
         smoke = Instantiate(Resources.Load<GameObject>("WhiteSmoke"));
         float y = GetComponent<SpriteRenderer>().bounds.extents.y;
         smoke.transform.position = new Vector2(transform.position.x, transform.position.y - y);
+        smoke.transform.localScale = new Vector3(
+            GetComponent<SpriteRenderer>().bounds.extents.x / 6, 
+            transform.localScale.y,
+            transform.localScale.z);
     }
 
     internal void exorcise() {
