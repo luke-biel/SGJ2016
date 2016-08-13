c4867bf474ea7c84dfe644d722d6d2115c776e03
Jakub <qbamix@gmail.com>
2016-08-13 17:01:34 +0200

    zmiany które musiałem zrobić by gra poprawnie wyglądała (przynajmniej) na standalone x86

---
+++ Assets/quit.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class quit : MonoBehaviour {
+	
+	void FixedUpdate () {
+		if (Input.GetButtonDown ("Cancel"))
+			Application.Quit ();
+	}
+}
